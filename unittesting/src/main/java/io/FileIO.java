de6f22467912c220ac23c3a747577f59315ff004
stamatiap <stampapag7@gmail.com>
2021-05-05 23:01:10 +0300

    change readFile method so it ignores invalid entries

--- unittesting/src/main/java/io/FileIO.java
+++ unittesting/src/main/java/io/FileIO.java
@@ -14,12 +14,12 @@ import java.util.List;
 *
 * @author  agkortzis
 * @version 1.0
-* @since   2020-04-06 
+* @since   2020-04-06
 */
 public class FileIO {
-	
+
 	/**
-	 * Reads a file that contains numbers line by line 
+	 * Reads a file that contains numbers line by line
 	 * and returns an array of the integers found in the file.
 	 * @param filepath the file that contains the numbers
 	 * @return an array of numbers
@@ -29,26 +29,33 @@ public class FileIO {
 	 */
 	public int[] readFile(String filepath) {
 		File file = new File(filepath);
-		if (!file.exists()) 
+		if (!file.exists())
 			throw new IllegalArgumentException("Input file does not exist");
-		
+
 		List<Integer> numbersList = new ArrayList<>();
 		BufferedReader reader;
 		try {
 			reader = new BufferedReader(new FileReader(file));
 			String line = null;
 			while ((line = reader.readLine()) != null) {
-				int number = Integer.parseInt(line);
-				numbersList.add(number);
+				boolean parsable = false;
+				try {
+					Integer.parseInt(line);
+					parsable = true;
+				} catch (final NumberFormatException e) { }
+				if (parsable) {
+					int number = Integer.parseInt(line);
+					numbersList.add(number);
+				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		if (numbersList.size() == 0) 
+
+		if (numbersList.size() == 0)
 			throw new IllegalArgumentException("Given file is empty");
-		
-		// Convert a List to an array using 
+
+		// Convert a List to an array using
 		return numbersList.stream().mapToInt(i -> i).toArray();
 	}
 
