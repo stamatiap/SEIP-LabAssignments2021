71c9c660007333c4fb9bca126a157becbde3408e
stamatiap <stampapag7@gmail.com>
2021-05-05 16:44:31 +0300

    create MyMathTest class

---
+++ unittesting/src/test/java/math/MyMathTest.java
@@ -0,0 +1,27 @@
+package math;
+
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.Rule;
+
+public class MyMathTest {
+	MyMath mm = new MyMath();
+	
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
+	@Test
+	public void test_factorial_negative() {
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("number should between 0 and 12");
+		mm.factorial(-7);
+	}
+	
+	@Test
+	public void test_factorial_overLimit() {
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("number should between 0 and 12");
+		mm.factorial(21);
+	}
+
+}
