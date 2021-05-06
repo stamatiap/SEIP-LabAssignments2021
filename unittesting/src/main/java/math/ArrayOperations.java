80881ec03e838e9b51e699939c25050ade40fe2e
stamatiap <stampapag7@gmail.com>
2021-05-06 12:16:09 +0300

    create ArrayOperations class

---
+++ unittesting/src/main/java/math/ArrayOperations.java
@@ -0,0 +1,27 @@
+package math;
+
+import io.FileIO;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayOperations {
+	
+	public int[] findPrimesInFile(FileIO fileIo, String filepath, MyMath myMath) {
+		int[] numbers = fileIo.readFile(filepath);
+		MyMath mm = new MyMath();
+		List<Integer> selectedNumbers = new ArrayList<>();
+		
+		for (int num : numbers) {
+			if (mm.isPrime(num)) {
+				selectedNumbers.add(num);
+			}
+		}
+		
+		int [] selectedNums = new int[selectedNumbers.size()];
+		for (int i = 0; i <= selectedNumbers.size(); i++) {
+			selectedNums[i] = selectedNumbers.get(i);
+		}
+		
+		return selectedNums;
+	}
+}
