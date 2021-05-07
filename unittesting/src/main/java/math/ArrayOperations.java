5e1fd32509ca4e96ede729b5e2012914ed1a83f2
stamatiap <stampapag7@gmail.com>
2021-05-07 11:17:57 +0300

    add javadoc

--- unittesting/src/main/java/math/ArrayOperations.java
+++ unittesting/src/main/java/math/ArrayOperations.java
@@ -4,8 +4,21 @@ import io.FileIO;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+* The ArrayOperations provides a method
+* that returns the prime numbers in a file
+*
+* @author  stamatiap
+*/
 public class ArrayOperations {
-	
+	/**
+	 * This method receives the path of a file, reads the file and
+	 * returns all the prime numbers that are in it
+	 * 
+	 * @param fileio FileIO object
+	 * @param filepath String, the path of the file
+	 * @param myMath MyMath object
+	 */
 	public int[] findPrimesInFile(FileIO fileIo, String filepath, MyMath myMath) {
 		int[] numbers = fileIo.readFile(filepath);
 		MyMath mm = new MyMath();
