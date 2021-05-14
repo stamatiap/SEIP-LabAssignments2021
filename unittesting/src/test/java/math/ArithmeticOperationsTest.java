05ab66b942dc85ce915be1812fd8aa9b8b7fd95d
stamatiap <stampapag7@gmail.com>
2021-05-14 12:45:44 +0300

    add test cases in arithemetic operations test class - issue #10

--- unittesting/src/test/java/math/ArithmeticOperationsTest.java
+++ unittesting/src/test/java/math/ArithmeticOperationsTest.java
@@ -16,8 +16,24 @@ public class ArithmeticOperationsTest {
 	@Test
 	public void test_divide_result() {
 		Assert.assertEquals(10, ao.divide(100, 10), 0.001);
+		Assert.assertEquals(0.1, ao.divide(10, 100), 0.001);
+		Assert.assertEquals(-2, ao.divide(-8, 4), 0.001);
+		Assert.assertEquals(-0.5, ao.divide(4, -8), 0.001);
 	}
 	
+	@Test
+	public void test_divide_zero_result() {
+		Assert.assertEquals(0, ao.divide(0, 10), 0.001);
+		Assert.assertEquals(0, ao.divide(0, -10), 0.001);
+	}
+	
+	@Test
+	public void test_divide_borders() {
+		Assert.assertEquals(-1, ao.divide(-1, 1), 0.001);
+		Assert.assertEquals(-1, ao.divide(1, -1), 0.001);
+		Assert.assertEquals(1, ao.divide(1, 1), 0.001);
+	}
+
 	@Rule
 	public ExpectedException thrown = ExpectedException.none();
 	
@@ -62,5 +78,13 @@ public class ArithmeticOperationsTest {
 	@Test
 	public void test_multiply_result() {
 		Assert.assertEquals(200, ao.multiply(10, 20));
+	}
+
+	@Test
+	public void test_multiply_borders() {
+		Assert.assertEquals(7, ao.multiply(1, 7));
+		Assert.assertEquals(8, ao.multiply(8, 1));
+		Assert.assertEquals(Integer.MAX_VALUE, ao.multiply(Integer.MAX_VALUE, 1));
+		Assert.assertEquals(Integer.MAX_VALUE, ao.multiply(1, Integer.MAX_VALUE));
 	}
 }
